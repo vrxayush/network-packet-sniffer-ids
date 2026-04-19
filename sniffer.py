0233db6cf8108eb69f67237ea39afdc348bc0cda
Ayush <ayushgaming145@gmail.com>
2026-04-20 00:09:23 +0530

    Add packet processing function to sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,3 +1,13 @@
 from scapy.all import sniff, IP, TCP, UDP, ICMP
 from database import save_packet
 from analyzer import detect_port_scan
+def process_packet(packet):
+    if IP in packet:
+        ip_layer = packet[IP]
+
+        protocol = "OTHER"
+        port = None
+
+        if TCP in packet:
+            protocol = "TCP"
+            port = packet[TCP].dport
